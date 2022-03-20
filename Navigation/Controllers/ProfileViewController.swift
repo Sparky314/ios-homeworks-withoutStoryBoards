d2d580ee025772d4d241ec37a0952973b673c862
Sparky314 <sergey.davlitshin@gmail.com>
2022-03-20 12:11:19 +0200

    "post" variable deleted from the FeedViewController. Code style fixed.

--- Navigation/Controllers/ProfileViewController.swift
+++ Navigation/Controllers/ProfileViewController.swift
@@ -11,19 +11,6 @@ class ProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Задаем фоновый цвет
-                view.backgroundColor = .systemMint
+        view.backgroundColor = .lightGray
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
